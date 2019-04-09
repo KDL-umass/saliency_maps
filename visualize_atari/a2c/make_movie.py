e92c4db12db75026a69978fee4c65f1119a306b8
Akanksha Atrey <akanksha.atrey@gmail.com>
2019-04-08 21:29:40 -0400

    adding intervention logic

--- visualize_atari/a2c/make_movie.py
+++ visualize_atari/a2c/make_movie.py
@@ -2,29 +2,63 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl ; mpl.use("Agg")
 import matplotlib.animation as manimation
 
-import gym, os, sys, time, argparse
+import gym, os, sys, time, argparse, pickle
 
 from saliency_maps.visualize_atari.a2c.saliency import get_env_meta, score_frame, saliency_on_atari_frame, occlude
-from saliency_maps.visualize_atari.a2c.rollout import rollout
+from saliency_maps.visualize_atari.a2c.rollout import rollout, single_intervention_move_ball
 
-def make_movie(env_name, env, model, checkpoint='*.tar', num_frames=20, first_frame=0, resolution=75, \
-                save_dir='./saliency_maps/movies/', density=5, radius=5, prefix='default', overfit_mode=False):
+def make_movie(env_name, env, model, num_frames=20, first_frame=0, resolution=75, save_dir='./saliency_maps/movies/', \
+                density=5, radius=5, prefix='default', IVmoveball=False):
     
-    # set up dir variables and environment
-    load_dir = '{}{}/'.format('overfit-' if overfit_mode else '', env_name.lower())
-    meta = get_env_meta(env_name)
+    # generate file names
+    movie_title = "{}-{}-{}-{}.mp4".format(prefix, num_frames, env_name.lower(), 1)
+    history_title = "{}-{}-{}-{}.pkl".format(prefix, num_frames, env_name.lower(), 1)
+    movie_title, history_title = update_filenames(save_dir, movie_title, history_title)
 
     # get a rollout of the policy
-    movie_title = "{}-{}-{}.mp4".format(prefix, num_frames, env_name.lower())
-    action_trace_title = "{}-{}-{}.txt".format(prefix, num_frames, env_name.lower())
-    print('\tmaking movie "{}" using checkpoint at {}{}'.format(movie_title, load_dir, checkpoint))
     max_ep_len = first_frame + num_frames + 1
     history = rollout(model, env, max_ep_len=max_ep_len)
+    
+    # save history
+    filehandler = open(save_dir + history_title, 'wb') 
+    pickle.dump(history, filehandler)
 
-    # save action trace
-    with open(save_dir + action_trace_title, 'w') as f:
-        for item in history['actions']:
-            f.write("%s\n" % item)
+    # make movie!
+    _make_movie(env_name, env, model, movie_title, history, num_frames, first_frame, resolution, \
+                save_dir, density, radius, prefix)
+
+    if IVmoveball:
+        make_intervention_movie(env_name, env, model, history_title, num_frames, first_frame, resolution, \
+                                save_dir, density, radius)
+
+def make_intervention_movie(env_name, env, model, default_history_path, num_frames=20, first_frame=0, resolution=75, \
+                            save_dir='./saliency_maps/movies/', density=5, radius=5, prefix='IVmoveball', \
+                            IVmoveball=True):
+    if IVmoveball:
+        prefix = "IVmoveball"
+        # get interventional history
+        default_history_file = open(save_dir + default_history_path, 'rb') 
+        default_history = pickle.load(default_history_file)
+        history = single_intervention_move_ball(model, env, default_history, move_distance=3, intervene_step=20)
+
+        # generate file names
+        movie_title = "{}-{}-{}-{}.mp4".format(prefix, num_frames, env_name.lower(), default_history_path.split(".pkl")[0][-1:])
+        history_title = "{}-{}-{}-{}.pkl".format(prefix, num_frames, env_name.lower(), default_history_path.split(".pkl")[0][-1:])
+
+        # save history
+        filehandler = open(save_dir + history_title, 'wb') 
+        pickle.dump(history, filehandler)
+
+        # make movie!
+        _make_movie(env_name, env, model, movie_title, history, num_frames, first_frame, resolution, \
+                save_dir, density, radius, prefix)
+
+
+def _make_movie(env_name, env, model, movie_title, history, num_frames=20, first_frame=0, resolution=75, \
+                save_dir='./saliency_maps/movies/', density=5, radius=5, prefix='default'):
+    # set up dir variables and environment
+    load_dir = env_name.lower()
+    meta = get_env_meta(env_name)
 
     # make the movie!
     start = time.time()
@@ -42,8 +76,8 @@ def make_movie(env_name, env, model, checkpoint='*.tar', num_frames=20, first_fr
                 actor_saliency = score_frame(model, history, ix, radius, density, interp_func=occlude, mode='actor')
                 critic_saliency = score_frame(model, history, ix, radius, density, interp_func=occlude, mode='critic')
             
-                frame = saliency_on_atari_frame(actor_saliency, frame, fudge_factor=meta['actor_ff'], channel=2)
-                frame = saliency_on_atari_frame(critic_saliency, frame, fudge_factor=meta['critic_ff'], channel=0)
+                frame = saliency_on_atari_frame(actor_saliency, frame, fudge_factor=meta['actor_ff'], channel=2, sigma=1) #blue
+                frame = saliency_on_atari_frame(critic_saliency, frame, fudge_factor=meta['critic_ff'], channel=0, sigma=1) #red
 
                 plt.imshow(frame) ; plt.title(env_name.lower(), fontsize=15)
                 writer.grab_frame() ; f.clear()
@@ -52,11 +86,29 @@ def make_movie(env_name, env, model, checkpoint='*.tar', num_frames=20, first_fr
                 print('\ttime: {} | progress: {:.1f}%'.format(tstr, 100*i/min(num_frames, total_frames)), end='\r')
     print('\nfinished.')
 
+def update_filenames(directory, movie_file, history_file):
+    if os.path.isfile(directory + movie_file):
+        count = 1
+        while True:
+            count += 1
+            new_movie_file = movie_file.split(".mp4")[0][:-1] + str(count) + ".mp4"
+            new_history_file = history_file.split(".pkl")[0][:-1] + str(count) + ".pkl"
+            if os.path.isfile(directory + new_movie_file):
+                continue
+            else:
+                movie_file = new_movie_file
+                history_file = new_history_file
+                break
+
+    return movie_file, history_file
+
 # user might also want to access make_movie function from some other script
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description=None)
-    parser.add_argument('-e', '--env', default='Breakout-v0', type=str, help='gym environment')
+    parser.add_argument('-en', '--env_name', default='BreakoutToyboxNoFrameskip-v4', type=str, help='name of gym environment')
+    parser.add_argument('-e', '--env', help='baselines environment object')
+    parser.add_argument('-m', '--model', help='baselines model object')
     parser.add_argument('-d', '--density', default=5, type=int, help='density of grid of gaussian blurs')
     parser.add_argument('-r', '--radius', default=5, type=int, help='radius of gaussian blur')
     parser.add_argument('-f', '--num_frames', default=20, type=int, help='number of frames in movie')
@@ -64,8 +116,6 @@ if __name__ == '__main__':
     parser.add_argument('-dpi', '--resolution', default=75, type=int, help='resolution (dpi)')
     parser.add_argument('-s', '--save_dir', default='./movies/', type=str, help='dir to save agent logs and checkpoints')
     parser.add_argument('-p', '--prefix', default='default', type=str, help='prefix to help make video name unique')
-    parser.add_argument('-c', '--checkpoint', default='*.tar', type=str, help='checkpoint name (in case there is more than one')
-    parser.add_argument('-o', '--overfit_mode', default=False, type=bool, help='analyze an overfit environment (see paper)')
     args = parser.parse_args()
 
     make_movie(args.env, args.checkpoint, args.num_frames, args.first_frame, args.resolution,
--- visualize_atari/a2c/rollout.py
+++ visualize_atari/a2c/rollout.py
@@ -1,6 +1,8 @@
 from baselines import run
 from baselines.common import atari_wrappers
 
+from toybox.interventions.breakout import *
+
 import numpy as np
 from scipy.misc import imresize # preserves single-pixel info _unlike_ img = img[::2,::2]
 
@@ -15,6 +17,65 @@ def rollout(model, env, max_ep_len=3e3):
     turtle = atari_wrappers.get_turtle(env)
     # This is a hack to get the starting screen, which throws an error in ALE for amidar
     num_steps = -1
+
+    while not done and episode_length <= max_ep_len:
+        episode_length += 1
+        actions, value, _, _, a_logits = model.step(obs)
+        num_lives = turtle.ale.lives()
+        obs, reward, done, info = env.step(actions)
+        epr += reward[0]
+        color_frame = turtle.toybox.get_rgb_frame()
+        #time.sleep(1.0/60.0)
+
+        #print(a_logits, value, actions)
+
+        #save info
+        history['ins'].append(obs)
+        history['a_logits'].append(a_logits)
+        history['values'].append(value)
+        history['actions'].append(actions[0])
+        history['color_frame'].append(color_frame)
+        print('\tstep # {}, reward {:.0f}'.format(episode_length, epr), end='\r')
+
+    return history
+
+def single_intervention_move_ball(model, env, rollout_history, max_ep_len=3e3, move_distance=1, intervene_step=20):
+    history = {'ins': [], 'a_logits': [], 'values': [], 'actions': [], 'color_frame': []}
+    episode_length, epr, done = 0, 0, False
+
+    #logger.log("Running trained model")
+    print("Running trained model")
+    obs = env.reset()
+    turtle = atari_wrappers.get_turtle(env)
+    tb = turtle.toybox
+    # This is a hack to get the starting screen, which throws an error in ALE for amidar
+    num_steps = -1
+
+    while episode_length < intervene_step:
+        obs, reward, done, info = env.step(rollout_history['actions'][episode_length])
+        epr += reward[0]
+        color_frame = turtle.toybox.get_rgb_frame()
+
+        #save info
+        history['ins'].append(obs)
+        history['a_logits'].append(rollout_history['a_logits'][episode_length])
+        history['values'].append(rollout_history['values'][episode_length])
+        history['actions'].append(rollout_history['actions'][episode_length])
+        history['color_frame'].append(color_frame)
+        print('\tstep # {}, reward {:.0f}'.format(episode_length, epr), end='\r')
+
+        episode_length += 1
+
+    print("Intervening now and forward simulating")
+    with BreakoutIntervention(tb) as intervention: 
+        ball_pos = intervention.get_ball_position()
+        print("old: ", ball_pos)
+        ball_pos['x'] = ball_pos['x'] + move_distance
+        ball_pos['y'] = ball_pos['y'] + move_distance
+        print("new: ", ball_pos)
+        intervention.set_ball_position(ball_pos)
+        ball_pos_post = intervention.get_ball_position()
+        assert ball_pos_post['x'] == ball_pos['x']
 
     while not done and episode_length <= max_ep_len:
         episode_length += 1
