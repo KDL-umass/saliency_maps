e7feefb49a53c54c48f4780a2131df50f7bbad30
Akanksha Atrey <akanksha.atrey@gmail.com>
2019-09-03 20:55:01 -0400

    refactored object saliency init file

--- object_saliency/__init__.py
+++ object_saliency/__init__.py
@@ -1,12 +1,12 @@
 RESOURCES_PATH = './saliency_maps/object_saliency/resources'
 
-AMIDAR_OBJECTS = {'player': RESOURCES_PATH+'/amidar'+'/player_l1.png',
-					'enemy': RESOURCES_PATH+'/amidar'+'/enemy_l1.png',
-					'tile_painted': RESOURCES_PATH+'/amidar'+'/block_tile_painted_l1.png',
-					'tile_unpainted': RESOURCES_PATH+'/amidar'+'/block_tile_unpainted_l1.png',
-					'tile_box_painted': RESOURCES_PATH+'/amidar'+'/painted_box_bar.png',
-					'player_chase': RESOURCES_PATH+'/amidar'+'/enemy_chase_l1.png'}
-AMIDAR_MOVING_OBJ = ['player', 'enemy', 'player_chase']
+AMIDAR_OBJECT_KEYS = ['player', 'enemy', 'tile_painted', 'tile_unpainted', 'tile_box_painted','player_chase']
+AMIDAR_OBJECT_TEMPLATES = {'player': RESOURCES_PATH+'/amidar'+'/player_l1.png',
+						'enemy': RESOURCES_PATH+'/amidar'+'/enemy_l1.png',
+						'tile_painted': RESOURCES_PATH+'/amidar'+'/block_tile_painted_l1.png',
+						'tile_unpainted': RESOURCES_PATH+'/amidar'+'/block_tile_unpainted_l1.png',
+						'tile_box_painted': RESOURCES_PATH+'/amidar'+'/painted_box_bar.png',
+						'player_chase': RESOURCES_PATH+'/amidar'+'/enemy_chase_l1.png'}
 
-BREAKOUT_OBJECTS = {}
-BREAKOUT_MOVING_OBJ = ['balls', 'paddle']
+BREAKOUT_OBJECT_KEYS = []
+BREAKOUT_OBJECT_TEMPLATES = {}
