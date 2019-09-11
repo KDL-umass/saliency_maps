1e5342564c642c912cf2f7e9059a53dd4c194958
Akanksha Atrey <akanksha.atrey@gmail.com>
2019-09-11 09:39:05 -0400

    scripts to automate pkl generation on gypsum

--- experiments/runIV_amidar_score.py
+++ experiments/runIV_amidar_score.py
@@ -1,19 +1,33 @@
 import os
 
 #NOTE: comment out the creation of MP4 files in make_movie.py when running this. 
+
+default = "python -m saliency_maps.visualize_atari.make_movie --env_name=AmidarToyboxNoFrameskip-v4 \
+        --alg=a2c --load_path=./models/AmidarToyboxNoFrameskip-v4/amidar4e7_a2c.model --num_frames=1000 --radius=2"
+
+IVdecrementScore = "python -m saliency_maps.visualize_atari.make_movie --env_name=AmidarToyboxNoFrameskip-v4 \
+        --alg=a2c --load_path=./models/AmidarToyboxNoFrameskip-v4/amidar4e7_a2c.model --num_frames=1000 --radius=2 \
+        --history_file=default-1000-amidartoyboxnoframeskip-v4-{}.pkl --IVdecrementScore=True"
+
+IVnonChangingScores = "python -m saliency_maps.visualize_atari.make_movie --env_name=AmidarToyboxNoFrameskip-v4 \
+        --alg=a2c --load_path=./models/AmidarToyboxNoFrameskip-v4/amidar4e7_a2c.model --num_frames=1000 --radius=2 \
+        --history_file=default-1000-amidartoyboxnoframeskip-v4-{}.pkl --IVnonChangingScores=True"
+
+IVmultModifyScores = "python -m saliency_maps.visualize_atari.make_movie --env_name=AmidarToyboxNoFrameskip-v4 \
+        --alg=a2c --load_path=./models/AmidarToyboxNoFrameskip-v4/amidar4e7_a2c.model --num_frames=1000 --radius=2 \
+        --history_file=default-1000-amidartoyboxnoframeskip-v4-{}.pkl --IVmultModifyScores=True"
+
 #run interventions for each of 50 samples of default file
-for i in range(5,56): 
+for i in range(1,51): 
+    #generate raw files
+    os.system(default)
+
+for i in range(1,51):
     #run IV decrement score
-    os.system("./start_python -m saliency_maps.visualize_atari.make_movie --env_name=AmidarToyboxNoFrameskip-v4 \
-        --alg=a2c --load_path=./models/AmidarToyboxNoFrameskip-v4/amidar4e7_a2c.model --num_frames=150 --radius=2 \
-        --history_file=default-150-amidartoyboxnoframeskip-v4-{}.pkl --IVdecrementScore=True".format(i))
+    os.system(IVdecrementScore.format(i))
 
     #run IV nonchanging scores
-    os.system("./start_python -m saliency_maps.visualize_atari.make_movie --env_name=AmidarToyboxNoFrameskip-v4 \
-        --alg=a2c --load_path=./models/AmidarToyboxNoFrameskip-v4/amidar4e7_a2c.model --num_frames=150 --radius=2 \
-        --history_file=default-150-amidartoyboxnoframeskip-v4-{}.pkl --IVnonChangingScores=True".format(i))
+    os.system(IVnonChangingScores.format(i))
 
     #run IV modifying scores
-    os.system("./start_python -m saliency_maps.visualize_atari.make_movie --env_name=AmidarToyboxNoFrameskip-v4 \
-        --alg=a2c --load_path=./models/AmidarToyboxNoFrameskip-v4/amidar4e7_a2c.model --num_frames=150 --radius=2 \
-        --history_file=default-150-amidartoyboxnoframeskip-v4-{}.pkl --IVmultModifyScores=True".format(i))
+    os.system(IVmultModifyScores.format(i))
