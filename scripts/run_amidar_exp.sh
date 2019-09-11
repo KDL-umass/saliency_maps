9ad3eee0b6c5b4c353569c46cd7f76f8fe72e488
Akanksha Atrey <akanksha.atrey@gmail.com>
2019-09-11 16:13:56 -0400

    updating gypsum script for amidar score exp

--- scripts/run_amidar_exp.sh
+++ scripts/run_amidar_exp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 #SBATCH --job-name=saliency_amidar
-#SBATCH --output=res_%j.txt  				# output file
-#SBATCH -e res_%j.err        				# File to which STDERR will be written
+#SBATCH --output=saliency_maps/scripts/output/res_%j.txt   				# output file
+#SBATCH -e saliency_maps/scripts/output/res_%j.err        				# File to which STDERR will be written
 #SBATCH --partition=titanx-long         	# Partition to submit to
 #
-#SBATCH --mem=32000                     	# Memory required in MB
+#SBATCH --mem=64000                     	# Memory required in MB
 #SBATCH --gres=gpu:1                    	# No. of required GPUs
-#SBATCH --ntasks-per-node=5            	# No. of cores required
+#SBATCH --ntasks-per-node=5            	    # No. of cores required
 #SBATCH --mem-per-cpu=20000             	# Memory in MB per cpu allocated
 
 echo "SLURM_JOBID: " $SLURM_JOBID
@@ -20,7 +20,7 @@ echo "LIBCTOYBOX $LIBCTOYBOX"
 
 source ~/.bashrc ; source activate tf
 
-python -m saliency_maps.experiments.plot_amidar_score_exp
+python -m saliency_maps.experiments.plot_amidar_score_exp -s='perturbation' -n=1000
 
 echo "Done"
 
